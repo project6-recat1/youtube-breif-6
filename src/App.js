64fd054727c4a81c5870fb7bf1bbd8e2f5ca8bc6
victoriaalnaimat <137334517+victoria-alnaimat@users.noreply.github.com>
2023-06-23 22:22:23 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import YouTubeVideo from './components/YouTubeVideo'; // Adjust this path according to your file structure
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About our Website</h1>
+      <YouTubeVideo />
+    </div>
+  );
+}
+
+export default App;
